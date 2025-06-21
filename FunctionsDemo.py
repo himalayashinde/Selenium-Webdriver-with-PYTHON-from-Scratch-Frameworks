cc2146d6a0a9dc5071d99705857c3eecf59aa2e6
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-06-21 23:57:43 +0530

    Function Demo Completed

---
+++ FunctionsDemo.py
@@ -0,0 +1,13 @@
+#Functions Demo in python
+
+#Function Declaration
+def greetme(name):
+    print("Hello "+ name)
+
+def AddIntegers(a,b):
+    print("Sum of Integers = ", a+b)
+
+# Function Call
+greetme("Rahul Shetty")
+
+AddIntegers(2,3)
