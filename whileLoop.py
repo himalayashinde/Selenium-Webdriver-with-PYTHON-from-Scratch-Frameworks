61d40b274543ee1b34b44aff605470fc96c133a1
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-06-21 23:33:28 +0530

    while loop completed

---
+++ whileLoop.py
@@ -0,0 +1,34 @@
+it=10
+
+# while it>=1:
+#     print(it)
+#     it= it-1
+#
+# print("While Loop Completed...!!!")
+
+# print("\n*************** Break *************************\n")
+#
+# while it>=1:
+#     if it==3:
+#         break
+#
+#     print(it)
+#     it= it-1
+#
+# print("While Loop Completed...!!!")
+
+
+print("\n*************** Continue *************************\n")
+
+while it>=1:
+    if it ==9:
+        it = it -1
+        continue
+
+    if it==3:
+        break
+
+    print(it)
+    it= it-1
+
+print("While Loop Completed...!!!")
