d253b32328b8e31b17cc39ba07edbacefc2bd59a
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-06-21 23:17:25 +0530

    Loops concepts started

---
+++ loops.py
@@ -0,0 +1,36 @@
+
+greetings = "Good Morning"
+
+if greetings == "Morning":
+    print("Condition Matches")
+else:
+    print("Condition do not match")
+
+print("If Else Completed")
+
+# For Loops
+
+obj = [2, 3, 5, 7, 9]
+
+for i in obj:
+    print(i)
+
+print("\n")
+
+for i in obj:
+    print(i*2)
+
+print("\n************* Summation *********************")
+
+summation =0
+
+for j in range(1,6):
+    summation =summation + j
+
+print(summation)
+
+print("\n**************** ODD Numbers ******************")
+
+for k in range(1,10,2):
+    print(k)
+
