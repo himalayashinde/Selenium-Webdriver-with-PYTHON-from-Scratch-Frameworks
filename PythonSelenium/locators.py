ef08bb4651bfeb0252e3ec9f4a310de8ab3261ab
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-06-26 00:10:47 +0530

    Locators need to be updated

---
+++ PythonSelenium/locators.py
@@ -0,0 +1,36 @@
+import os.path
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+base_dir= os.path.dirname(__file__)
+print(base_dir)
+driver_paths= os.path.join(base_dir,"chromedriver-win64","chromedriver.exe")
+print(driver_paths)
+
+service_obj1= Service(driver_paths)
+driver= webdriver.Chrome(service=service_obj1)
+
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+print(driver.title)
+print(driver.current_url)
+
+# Xpath used below for reference
+driver.find_element(By.XPATH,"(//input[@name='name'])[1]").send_keys("Himalaya Shinde")
+
+# Name tag
+driver.find_element(By.NAME,"email").send_keys("himalayashinde@gmail.com")
+
+# Id
+driver.find_element(By.ID,"exampleInputPassword1").send_keys("Himalaya@1234")
+driver.find_element(By.ID,"exampleCheck1").click()
+
+
+
+
+
+time.sleep(5)
