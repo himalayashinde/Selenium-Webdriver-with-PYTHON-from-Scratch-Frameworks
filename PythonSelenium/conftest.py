c4ac48a9b4ab4aa1d26fcec77ca1df5eefc97ca8
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-07-07 19:38:24 +0530

    Section 22 Completed

--- PythonSelenium/conftest.py
+++ PythonSelenium/conftest.py
@@ -1,9 +1,10 @@
+import os
 from email.policy import default
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+driver = None
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -12,6 +13,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browserInstance(request):
+    global driver
     browser_name= request.config.getoption("browser_name")
     if browser_name == "chrome":
         options = Options()
@@ -47,3 +49,31 @@ def browserInstance(request):
     driver.get("https://rahulshettyacademy.com/loginpagePractise/")
     yield driver    # Before Test function execution
     driver.close()  # Post your function execution
+
+@pytest.hookimpl( hookwrapper=True )
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin( 'html' )
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr( report, 'extra', [] )
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr( report, 'wasxfail' )
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
+            file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
+            print( "file name is " + file_name )
+            _capture_screenshot( file_name )
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append( pytest_html.extras.html( html ) )
+        report.extras = extra
+
+
+def _capture_screenshot(file_name):
+    driver.get_screenshot_as_file(file_name)
